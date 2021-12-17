5e933506e7d243e2830905f8b7c0622bc099e7dc
Liam Johnson <liam.gwjohnson@gmail.com>
2021-12-17 15:49:30 -0800

    cleaned up and added family-level tip

--- scripts/prep_phylogeny.r
+++ scripts/prep_phylogeny.r
@@ -10,10 +10,6 @@ library(V.PhyloMaker)
 library(Taxonstand)
 
 
-# define convenience functions
-source("/home/liam/Documents/MSc/analysis/misc_blerfLab/checkNames_function.r")
-
-
 # load species lists
 # FIA trees
 sp_tr <- read.csv("./output/FIAspp_toUse_2021-12-15T15:33:29.csv")$species
@@ -21,13 +17,17 @@ sp_tr <- read.csv("./output/FIAspp_toUse_2021-12-15T15:33:29.csv")$species
 # host trees from pest database
 sp_pest  <- read.csv("./output/pestDB-hostList_clean_2021-12-15T15:15:34.csv")$host
 
-# Reassign phylogeny for convenience
+# Reassign phylogeny for convenience (loaded with 'V.PhyloMaker' package)
 phy <- GBOTB.extended
 
+# tip list
 sp_phy <- phy$tip.label
 
 
-# # First step is to run a quick check that the phylogeny is as expected. The authors say that the tip list is standardized according to TPL; there are  74531 tips, so checking them all individually is inefficient. Instead, we'll sample a subset of 500 tips (0.7%) and check them against TPL.
+# First step is to run a quick check that the phylogeny is as expected.
+# The authors say that the tip list is standardized according to TPL; there are
+# 74531 tips, so checking them all individually is inefficient. Instead, we'll
+# sample a subset of 500 tips (0.7%) and check them against TPL.
 # 
 # tip_sample <- sample(GBOTB.extended$tip.label, 500)
 # 
@@ -47,24 +47,13 @@ sp_phy <- phy$tip.label
 # 
 # tip_sample_TPL[which(tip_sample_TPL$Taxonomic.status == "Synonym"),]
 # 
-# # Yup... not 100% 'accepted'. A full correction is out of scope at the moment, but we'll keep this problem in mind.
+# # Yup... not 100% 'accepted'. A full correction is out of scope at the
+# moment, but we'll keep this problem in mind.
 
 
-# # check overlap of species lists
-# length(unique(c(sp_tr, sp_pest))) #560 tree species
-# 
-# length(intersect(unique(c(sp_tr, sp_pest)),
-#                  sp_phy)) # 434 in phylogeny
-# 
-# setdiff(unique(c(sp_tr, sp_pest)),
-#         sp_phy)
-# 
-# 
-# length(sp_tr) #185
-# length(intersect(sp_tr, sp_phy)) #231    46 missing
-# 
-# length(sp_pest) #405
-# length(intersect(sp_pest, sp_phy)) #315    90 missing
+# check intersection of species lists
+length(setdiff(sp_pest, sp_phy)) # 90 hosts missing from phy
+length(setdiff(sp_tr, sp_phy)) # 46 FIA trees missing from phy
 
 
 # FIA tree spp are key, so we'll focus on those
@@ -83,119 +72,119 @@ sp_tr_mis_tpl <- Taxonstand::TPL(gsub("_", " ", sp_tr_mis),
                                  silent = TRUE,
                                  repeats = 6)
 
-# updated names
+# get list of updated/corrected names
 sp_tr_mis_tpl2 <- paste(sp_tr_mis_tpl$New.Genus,
                         sp_tr_mis_tpl$New.Species,
                         sep = "_")
 
-# species to add
+# check new intersection
+length(setdiff(sp_tr_mis_tpl2, sp_phy)) # 34 still missing
+# we'll have to add these tips
+
+# list of species to add
 sp_tr_add <- setdiff(sp_tr_mis_tpl2, sp_phy)
 
-# check if genera are present
-gen_tr_add <- unique(gsub("_.*", "", sp_tr_add))
+# ideally, a congener for each of these species will be present in the tree, and we will add the tips sister to their congeners
 
-# d<-vector(length=17)
-# for(i in 1:17){
-# d[i] <- length(grep(gen_tr_add[i], sp_phy))>=1
-# }
-# 
-# grep(gen_tr_add[14], sp_phy)
-# 
-# gen_tr_add[14]
-# all genera in phylogeny except gymnocladus
-sp_tr_add <- sp_tr_add[-which(sp_tr_add=="Gymnocladus_dioica")]
+# check for species whose genera are not present in the tree
+noCgen <- sp_tr_add[which(!gsub("_.*", "", sp_tr_add) %in%
+                              unique(gsub("_.*", "", sp_phy)))]
+
+# only one species affected - it will have to be added separately, at the base of its family
+
+# remove from list of tips to add
+sp_tr_add <- sp_tr_add[-which(sp_tr_add==noCgen)]
 
 
-
-# list of tips to keep in phy
+# get list of tips to keep in phy
 # overlap with pest
 t1 <- intersect(sp_pest, sp_phy)
 
 # native overlap with FIA
 t2 <- intersect(sp_tr, sp_phy)
 
-# corrected overlap with phy
+# corrected overlap with FIA
 t3 <- intersect(sp_tr_mis_tpl2, sp_phy)
 
 # congener from each of missing FIA genera
-ncgen <- setdiff(gen_tr_add,
+cGen <- setdiff(unique(gsub("_.*", "", sp_tr_add)),
                  unique(c(gsub("_.*", "", t2), gsub("_.*", "", t3))))
 
-#remove gymnocladus
-ncgen <- ncgen[-4]
+t4 <- vector(length=length(cGen))
 
-t4 <- vector(length=length(ncgen))
-
-for(i in seq_along(ncgen)){
-    t4[i] <- sp_phy[grep(ncgen[i], sp_phy)[1]]
+for(i in seq_along(cGen)){
+    t4[i] <- sp_phy[grep(cGen[i], sp_phy)[1]]
 }
 
 # combine
-finalTips <- c(t1, t2, t3, t4)
+finalTips <- c(t1, t2, t3, t4, noCgen)
+
+
+# add no-congener tip before pruning
+# get family
+fam <- subset(sp_tr_mis_tpl, New.Genus == gsub("_.*", "", noCgen))$Family
+
+# ugh, family is "Leguminosae", AKA "Fabaceae" - rename
+fam <-  "Fabaceae"
+
+# get mrca node
+fnode <- getMRCA(phy, tips.info[which(tips.info$family == fam), "species"])
+
+# add tip
+phy <- bind.tip(phy,
+                tip.label = noCgen,
+                where = fnode,
+                position = 0
+)
+
 
 # prune phy
 ourPhy <- keep.tip(phy, finalTips)
 
 
-# add missing tips
-# added as polytoies at base of genus
+# add missing tips with congeners
+# added as polytomies at base of genus
 # if only one congeneric tip is present, new mrca node is created at the 1/2 point along the pendant edge
 
 for(i in seq_along(sp_tr_add)){
     
     node <- getMRCA(ourPhy,
                     ourPhy$tip.label[grep(strsplit(sp_tr_add[i], "_")[[1]][1],
-                                          ourPhy$tip.label)])
+                                          ourPhy$tip.label)]
+                    )
     
     if(!is.null(node)){
-    
-ourPhy<-    bind.tip(ourPhy, tip.label = sp_tr_add[i],
-             where = node,
-             position = 0
-             )
+        
+        ourPhy <- bind.tip(ourPhy,
+                           tip.label = sp_tr_add[i],
+                           where = node,
+                           position = 0
+                           )
+    }else{
+        
+        ourPhy <- bind.tip(ourPhy,
+                           tip.label = sp_tr_add[i],
+                           
+                           where = grep(strsplit(sp_tr_add[i], "_")[[1]][1],
+                                        ourPhy$tip.label),
+                           
+                           position = ourPhy$edge.length[
+                               which(ourPhy$edge[,2] == grep(
+                                   strsplit(sp_tr_add[i], "_")[[1]][1],
+                                   ourPhy$tip.label))] / 2
+                           )
+    }
 }
 
-if(is.null(node)){
-    
-    ourPhy<-    bind.tip(ourPhy, tip.label = sp_tr_add[i],
-                         where = grep(strsplit(sp_tr_add[i], "_")[[1]][1],
-                                      ourPhy$tip.label),
-                         position = ourPhy$edge.length[which(ourPhy$edge[,2]==grep(strsplit(sp_tr_add[i], "_")[[1]][1], ourPhy$tip.label))]/2
-    )
-}
-}
 
-
-# # check plot
+# check plot
 # tree <- ourPhy
-# sister <- "Magnolia_montana" 
+# sister <- noCgen #"Magnolia_montana"
 # plot(keep.tip(tree, c(c(which(tree$tip.label==sister)-2):c(which(tree$tip.label==sister)+2))[which(c(c(which(tree$tip.label==sister)-2):c(which(tree$tip.label==sister)+2))>0)]))
-# 
 
 
-
-# add one remaining tip?
-
-
-# drop t4 tips
+# remove t4 tips (congeners used to locate tip addition, no longer needed)
 ourPhy <- drop.tip(ourPhy, t4)
-
-
-## this leads to multi-naming issues! will change names of FIA layers instead
-# # change names to match FIA key
-# key <- subset(sp_tr_mis_tpl, Taxon %in% gsub("_", " ", setdiff(sp_tr, ourPhy$tip.label)))
-# 
-# key$newname <- paste(key$New.Genus, key$New.Species, sep = "_")
-# 
-# key <- key[,c("Taxon", "newname")]
-# 
-# key$Taxon <- gsub(" ", "_", key$Taxon)
-# 
-# for(i in seq_along(ourPhy$tip.label)){
-#     if(ourPhy$tip.label[i]%in%key$newname){
-#         ourPhy$tip.label[i] <- key$Taxon[which(key$newname==ourPhy$tip.label[i])]
-#     }
-# }
 
 
 # write to file
@@ -203,3 +192,37 @@ write.tree(ourPhy,
            paste0("./output/focalPhy_",
                   gsub(" ", "T", Sys.time()),
                   ".nwk"))
+
+
+
+
+###############################################################################
+# make key to connect FIA species with tips in cases where we renamed tips
+
+## this needs cleaning up
+
+# library(dplyr)
+# 
+# fia_tax <- read.csv("./indata/studyregion_imputed_FIA_species_by_plot_summary_good_taxonomy.csv")
+# 
+# new_tax <- subset(sp_tr_mis_tpl, Taxon %in% gsub("_", " ", setdiff(sp_tr, ourPhy$tip.label)), select = c("Taxon", "New.Genus", "New.Species"))
+# 
+# all_tax <- left_join(new_tax, fia_tax, by = c("Taxon" = "UpdatedFIABinomial"))
+# 
+# all_tax <- unique(all_tax[,c("Taxon", "New.Genus", "New.Species", "SPCD")])
+# 
+# all_tax$tip_name <- paste(all_tax$New.Genus, all_tax$New.Species, sep="_")
+# 
+# all_tax <- all_tax[,c("Taxon", "tip_name", "SPCD")]
+# 
+# names(all_tax)[1] <- "UpdatedFIABinomial"
+# 
+# rest_tax <- subset(fia_tax, !SPCD%in%all_tax$SPCD)
+# 
+# rest_tax <- unique(rest_tax[,c("UpdatedFIABinomial", "SPCD")])
+# 
+# rest_tax$tip_name <- gsub(" ", "_", rest_tax$UpdatedFIABinomial)
+# 
+# rest_tax[grep(" x ", rest_tax$UpdatedFIABinomial), "tip_name"] <- NA
+# 
+# tax <- rbind(all_tax, rest_tax)
